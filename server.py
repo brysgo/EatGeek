492398509ff15b848e633596487adb9aa709f3ef
Bryan Goldstein <brysgo@gmail.com>
2018-12-02 12:10:45 -0500

    fake data in flask

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Welcome to EatGeek!'
 
+
 @app.route('/nutrition')
 def nutrition():
-    return jsonify({ "sugar": "0-7 g", "carbohydrates": "0-10 g" })
+    description = request.args.get('description')
 
+    return {
+        "Spinach Florentine Flatbread spinach artichoke hearts monterey jack parmesan romano tomatoes garlic fresh basil crushed red pepper oregano": jsonify({"sugar": "4 g", "carbohydrates": "51 g"}),
+        "BBQ Chicken Flatbread chipotle chicken bbq sauce monterey jack cheddar cilantro red peppers red onions": jsonify({"sugar": "18 g", "carbohydrates": "66 g"}),
+        "Crispy Brussels Sprouts lemon soy vinaigrette roasted onions croutons cotija cheese": jsonify({"sugar": "8 g", "carbohydrates": "38 g"}),
+        "Giant Onion Rings bbq ranch": jsonify({"sugar": "33 g", "carbohydrates": "155 g"}),
+        "none": jsonify({"sugar": "0-7 g", "carbohydrates": "0-10 g"})
+    }[description or "none"]
